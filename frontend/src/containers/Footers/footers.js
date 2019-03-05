a6c01f990f458641850d4000a9514ea756bed0ef
Nibesh Khadka <nibeshkhadka@gmail.com>
2019-03-05 04:31:34 +0200

    Initial Footer Components

--- frontend/src/containers/Footers/footers.js
+++ frontend/src/containers/Footers/footers.js
@@ -2,7 +2,13 @@ import React, { Component } from "react";
 
 class Footers extends Component {
   render() {
-    return <h1>Footers</h1>;
+    return (
+      <div>
+        <footer className="bg-dark text-white mt-5 p-4 text-center">
+          Copyright &copy {new Date().getFullYear()} MovieWatch
+        </footer>
+      </div>
+    );
   }
 }
 
