30d64263b7602084303487b69e994dad9e63ad67
Nibesh Khadka <nibeshkhadka@gmail.com>
2019-03-11 15:03:45 +0200

    Top pick section made dynamic ver 1

--- frontend/src/components/TopPicks/topPicks.js
+++ frontend/src/components/TopPicks/topPicks.js
@@ -1,19 +1,32 @@
 import React, { Component } from "react";
 import WithClass from "../../HOC/ReactAux";
-
+import Classes from "./topPicks.css";
 class TopPicks extends Component {
+  state = {
+    topMoviePicks: this.props.topPicks
+  };
+
   render() {
-    return (
-      <WithClass>
-        <div className="card" style={{ width: 18 + "rem" }}>
+    let topMovies = <p>No TopPicks Available</p>;
+    if (this.state.topMoviePicks) {
+      topMovies = (
+        <div className={"card ".concat(Classes.TopPicks_Card)}>
+          <div className="card-header">TOP PICKS</div>
           <ul className="list-group list-group-flush">
-            <li className="list-group-item">Cras justo odio</li>
-            <li className="list-group-item">Dapibus ac facilisis in</li>
-            <li className="list-group-item">Vestibulum at eros</li>
+            {this.state.topMoviePicks.map(movie => (
+              <li key={movie._id} className="list-group-item">
+                <img
+                  src={movie.image_url}
+                  className={"img-responsive ".concat(Classes.Image)}
+                />
+                {movie.title}
+              </li>
+            ))}
           </ul>
         </div>
-      </WithClass>
-    );
+      );
+    }
+    return <WithClass>{topMovies}</WithClass>;
   }
 }
 
